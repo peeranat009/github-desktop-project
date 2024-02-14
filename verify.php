3b6a46fd39aad91f7af8ac9ad07b2fe5343bc813
peeranat009 <159885131+peeranat009@users.noreply.github.com>
2024-02-14 11:06:45 +0700

    Work4

---
+++ verify.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 style="text-align: center;">Webboard Nine</h1>
+    <hr><br>
+    เข้าสู่ระบบด้วย <br>
+    Login =  <?php echo $_POST["login"];?>
+    Password =  <?php echo $_POST["pwd"];?>
+</body>
+</html>
